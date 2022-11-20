190f8026bdd3f2d85df2de258ce34e85e0e39271
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-20 16:40:40 +0500

    info about new added

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string Method4(int count, string text)
+{
+    string result =String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "x");
+Console.WriteLine(res);
