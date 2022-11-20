5b3f46468fcdede417ceda410650cded7c0b62cd
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-20 16:36:50 +0500

    info about Method2 added

---
+++ Method2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Text messege");*/
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21("Text messege", 4);
+//Method21(msg: "Text messege", count: 4);
+Method21(count: 4, msg: "Text messege");
